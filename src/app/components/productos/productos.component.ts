803be037cc8750c8245595c016871c790a0031f8
arcba <alejandroreyc@yahoo.com.ar>
2021-06-23 17:56:18 -0300

    Initial commit

---
+++ src/app/components/productos/productos.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ProductosModel } from '../../models/producto';
+import { ModalDialogService } from '../../services/modal-dialog.service';
+import { ProductosService } from '../../services/productos.service';
+
+@Component({
+  selector: 'app-productos',
+  templateUrl: './productos.component.html',
+  styleUrls: ['./productos.component.css']
+})
+export class ProductosComponent implements OnInit {
+
+  Lista: ProductosModel[] = [];
+  FormReg: FormGroup;
+  mostrar: boolean = false;
+  constructor(
+    public formBuilder: FormBuilder,
+    private ProductosService: ProductosService,
+    private modalDialogService: ModalDialogService
+  ) {}
+  ngOnInit() {
+    this.mostrarTabla();
+    this.createForm();
+  }
+  mostrarTabla() {
+    this.ProductosService.get().subscribe((res: any) => {
+      this.Lista = res;
+    });
+  }
+  Agregar() {
+    this.mostrar = true;
+  }
+  createForm() {
+    this.FormReg = this.formBuilder.group({
+      ProductoID: [0],
+      ProductoNombre: ['', [Validators.required]],
+      ProductoFechaAlta: [null, [Validators.required]],
+      ProductoStock: [0, [Validators.required]]
+    });
+  }
+  Grabar() {
+    if (this.FormReg.invalid) {
+      return;
+    }
+    const formValue = this.FormReg.value;
+    if (formValue.ProductoID == 0 || formValue.ProductoID == null) {
+      formValue.ProductoID = 0;
+      this.ProductosService.post(formValue).subscribe((res: any) => {
+        this.Volver();
+        this.modalDialogService.Alert('Registro agregado correctamente.');
+        this.mostrarTabla();
+      });
+    }
+  }
+  Volver() {
+    this.mostrar = false;
+  }
+
+  }
